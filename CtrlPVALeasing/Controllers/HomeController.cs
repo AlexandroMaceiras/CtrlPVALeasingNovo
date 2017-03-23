06043441fb4c8adf4e7aa00029a3acfe9f45622e
Alexandro Maceiras <alexandro@maceiras.com.br>
2017-03-23 16:15:10 -0300

    Add project files.

---
+++ CtrlPVALeasing/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CtrlPVALeasing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A página de descrição da sua aplicação.  ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Sua página de contato.";
+
+            return View();
+        }
+    }
+}
