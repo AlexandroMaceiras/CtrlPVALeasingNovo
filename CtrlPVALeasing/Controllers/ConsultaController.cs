043a2ff3c57f61ed5d46eb56375f1c32474946bf
Alexandro Maceiras <alexandro@maceiras.com.br>
2017-04-04 16:38:08 -0300

    Mais retoques e aceleração da pesquisa em LINQ

--- CtrlPVALeasing/Controllers/ConsultaController.cs
+++ CtrlPVALeasing/Controllers/ConsultaController.cs
@@ -147,99 +147,107 @@ namespace CtrlPVALeasing.Controllers
                 return RedirectToAction("ConsultaContrato");
             }
 
-                        model = (from a in db.Arm_LiquidadosEAtivos_Contrato
-                     join b in db.Arm_Veiculos
-                     on a.contrato equals b.contrato
-                     select new
-                     {
-                         id = a.id,
-                         contrato   = a.contrato,
-                         tipo = a.tipo,
-                         agencia = a.agencia,
-                         dta_inicio_contrato = a.dta_inicio_contrato,
-                         dta_vecto_contrato = a.dta_vecto_contrato,
-                         origem = a.origem,
-                         cpf_cnpj_cliente = a.cpf_cnpj_cliente,
-                         nome_cliente = a.nome_cliente,
-                         ddd_cliente_particular = a.ddd_cliente_particular,
-                         fone_cliente_particular = a.fone_cliente_particular,
-                         rml_cliente_particular = a.rml_cliente_particular,
-                         end_cliente = a.end_cliente,
-                         bairro_cliente = a.bairro_cliente,
-                         cidade_cliente = a.cidade_cliente,
-                         uf_cliente = a.uf_cliente,
-                         cep_cliente = a.cep_cliente,
-                         filler = a.filler,
-                         ddd_cliente_cml = a.ddd_cliente_cml,
-                         fone_cliente_cml = a.fone_cliente_cml,
-                         dta_ultimo_pagto = a.dta_ultimo_pagto,
-                         tipo_de_baixa = a.tipo_de_baixa,
-                         data_da_baixa = a.data_da_baixa,
-                         cod_empresa = a.cod_empresa,
-                         num_end_cliente = a.num_end_cliente,
-                         comp_end_cliente = a.comp_end_cliente,
-                         status = a.status,
+            model = (from a in db.Arm_LiquidadosEAtivos_Contrato
+            join b in db.Arm_Veiculos
+            on a.contrato equals b.contrato
+            where a.contrato.Equals(contrato)
+            where a.origem.Equals("B")
+            where b.origem.Equals("RECIBO VEN")
+            select new
+            {
+                id = a.id,
+                contrato   = a.contrato,
+                tipo = a.tipo,
+                agencia = a.agencia,
+                dta_inicio_contrato = a.dta_inicio_contrato,
+                dta_vecto_contrato = a.dta_vecto_contrato,
+                origem = a.origem,
+                cpf_cnpj_cliente = a.cpf_cnpj_cliente,
+                nome_cliente = a.nome_cliente,
+                ddd_cliente_particular = a.ddd_cliente_particular,
+                fone_cliente_particular = a.fone_cliente_particular,
+                rml_cliente_particular = a.rml_cliente_particular,
+                end_cliente = a.end_cliente,
+                bairro_cliente = a.bairro_cliente,
+                cidade_cliente = a.cidade_cliente,
+                uf_cliente = a.uf_cliente,
+                cep_cliente = a.cep_cliente,
+                filler = a.filler,
+                ddd_cliente_cml = a.ddd_cliente_cml,
+                fone_cliente_cml = a.fone_cliente_cml,
+                dta_ultimo_pagto = a.dta_ultimo_pagto,
+                tipo_de_baixa = a.tipo_de_baixa,
+                data_da_baixa = a.data_da_baixa,
+                cod_empresa = a.cod_empresa,
+                num_end_cliente = a.num_end_cliente,
+                comp_end_cliente = a.comp_end_cliente,
+                status = a.status,
 
-                         contrato_v = b.contrato,
-                         tipo_registro = b.tipo_registro,
-                         marca = b.marca,
-                         modelo = b. modelo,
-                         tipo_v = b.tipo,
-                         ano_fab = b.ano_fab,
-                         ano_mod = b.ano_mod,
-                         cor = b.cor,
-                         renavam = b.renavam,
-                         chassi = b.chassi,
-                         placa = b.placa,
-                         origem_v = b.origem//,
-                         //status_v = b.status
-                     }).AsEnumerable().Select(x => new ContratosVeiculosViewModel
-                     {
-                         id = x.id,
-                         contrato = x.contrato,
-                         tipo = x.tipo,
-                         agencia = x.agencia,
-                         dta_inicio_contrato = x.dta_inicio_contrato,
-                         dta_vecto_contrato = x.dta_vecto_contrato,
-                         origem = x.origem,
-                         cpf_cnpj_cliente = x.cpf_cnpj_cliente,
-                         nome_cliente = x.nome_cliente,
-                         ddd_cliente_particular = x.ddd_cliente_particular,
-                         fone_cliente_particular = x.fone_cliente_particular,
-                         rml_cliente_particular = x.rml_cliente_particular,
-                         end_cliente = x.end_cliente,
-                         bairro_cliente = x.bairro_cliente,
-                         cidade_cliente = x.cidade_cliente,
-                         uf_cliente = x.uf_cliente,
-                         cep_cliente = x.cep_cliente,
-                         filler = x.filler,
-                         ddd_cliente_cml = x.ddd_cliente_cml,
-                         fone_cliente_cml = x.fone_cliente_cml,
-                         dta_ultimo_pagto = x.dta_ultimo_pagto,
-                         tipo_de_baixa = x.tipo_de_baixa,
-                         data_da_baixa = x.data_da_baixa,
-                         cod_empresa = x.cod_empresa,
-                         num_end_cliente = x.num_end_cliente,
-                         comp_end_cliente = x.comp_end_cliente,
-                         status = x.status,
+                contrato_v = b.contrato,
+                tipo_registro = b.tipo_registro,
+                marca = b.marca,
+                modelo = b. modelo,
+                tipo_v = b.tipo,
+                ano_fab = b.ano_fab,
+                ano_mod = b.ano_mod,
+                cor = b.cor,
+                renavam = b.renavam,
+                chassi = b.chassi,
+                placa = b.placa,
+                origem_v = b.origem//,
+                //status_v = b.status
+            }).AsEnumerable().Select(x => new ContratosVeiculosViewModel
+            {
+                id = x.id,
+                contrato = x.contrato,
+                tipo = x.tipo,
+                agencia = x.agencia,
+                dta_inicio_contrato = x.dta_inicio_contrato,
+                dta_vecto_contrato = x.dta_vecto_contrato,
+                origem = x.origem,
+                cpf_cnpj_cliente = x.cpf_cnpj_cliente,
+                nome_cliente = x.nome_cliente,
+                ddd_cliente_particular = x.ddd_cliente_particular,
+                fone_cliente_particular = x.fone_cliente_particular,
+                rml_cliente_particular = x.rml_cliente_particular,
+                end_cliente = x.end_cliente,
+                bairro_cliente = x.bairro_cliente,
+                cidade_cliente = x.cidade_cliente,
+                uf_cliente = x.uf_cliente,
+                cep_cliente = x.cep_cliente,
+                filler = x.filler,
+                ddd_cliente_cml = x.ddd_cliente_cml,
+                fone_cliente_cml = x.fone_cliente_cml,
+                dta_ultimo_pagto = x.dta_ultimo_pagto,
+                tipo_de_baixa = x.tipo_de_baixa,
+                data_da_baixa = x.data_da_baixa,
+                cod_empresa = x.cod_empresa,
+                num_end_cliente = x.num_end_cliente,
+                comp_end_cliente = x.comp_end_cliente,
+                status = x.status,
 
-                         contrato_v = x.contrato,
-                         tipo_registro = x.tipo_registro,
-                         marca = x.marca,
-                         modelo = x.modelo,
-                         tipo_v = x.tipo,
-                         ano_fab = x.ano_fab,
-                         ano_mod = x.ano_mod,
-                         cor = x.cor,
-                         renavam = x.renavam,
-                         chassi = x.chassi,
-                         placa = x.placa,
-                         origem_v = x.origem//,
-                         //status_v = x.status_v
+                contrato_v = x.contrato,
+                tipo_registro = x.tipo_registro,
+                marca = x.marca,
+                modelo = x.modelo,
+                tipo_v = x.tipo,
+                ano_fab = x.ano_fab,
+                ano_mod = x.ano_mod,
+                cor = x.cor,
+                renavam = x.renavam,
+                chassi = x.chassi,
+                placa = x.placa,
+                origem_v = x.origem_v//,
+                //status_v = x.status_v
 
-                     }).Where(x => x.contrato.Equals(contrato)).Where(x => x.status.Equals(true)).ToList();
+            }).ToList();
+
+            //model = model.Where(s => s.contrato.Equals(contrato));
+            //model = model.Where(b => b.origem.Equals("B"));
+            //model = model.Where(w => w.origem_v.Contains("RECIBO VEN"));
 
             //return View(db.Arm_LiquidadosEAtivos_Contrato.Take(5).ToList());
+
             if (model.Count() == 0 || model == null)
             {
                 return RedirectToAction("ConsultaContrato");
