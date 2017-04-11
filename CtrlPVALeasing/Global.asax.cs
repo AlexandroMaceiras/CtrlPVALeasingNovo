287fbdae22e9983f2ddb510d533277bc0a982ff5
Alexandro Maceiras <alexandro@maceiras.com.br>
2017-04-11 17:29:18 -0300

    Só subindo o que deu certo

--- CtrlPVALeasing/Global.asax.cs
+++ CtrlPVALeasing/Global.asax.cs
@@ -16,6 +16,35 @@ namespace CtrlPVALeasing
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ModelBinders.Binders.Add(typeof(DateTime?), new DateTimeModelBinder());
+            ModelBinders.Binders.Add(typeof(DateTime), new DateTimeModelBinder());
+        }
+        public class DateTimeModelBinder : DefaultModelBinder
+        {
+            public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+            {
+                object result = null;
+
+                string modelName = bindingContext.ModelName;
+                string attemptedValue = bindingContext.ValueProvider.GetValue(modelName).AttemptedValue;
+
+                if (String.IsNullOrEmpty(attemptedValue))
+                {
+                    return result;
+                }
+
+                try
+                {
+                    result = DateTime.Parse(attemptedValue);
+                }
+                catch (FormatException e)
+                {
+                    bindingContext.ModelState.AddModelError(modelName, e);
+                }
+
+                return result;
+            }
         }
     }
 }
