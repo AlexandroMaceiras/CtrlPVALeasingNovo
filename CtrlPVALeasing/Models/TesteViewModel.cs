15702dda59342f776affc4f674bc945288d46e83
Alexandro Maceiras <alexandro@maceiras.com.br>
2017-05-29 02:51:58 -0300

    Mudança dos gráficos feitos em HOME

---
+++ CtrlPVALeasing/Models/TesteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace CtrlPVALeasing.Models
+{
+    public class TesteViewModel
+    {
+        public bool? status { get; set; }
+
+        public int? total { get; set; }
+
+        public bool? status2 { get; set; }
+
+        public int? total2 { get; set; }
+    }
+}
